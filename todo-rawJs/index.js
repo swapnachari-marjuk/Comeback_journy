7deb3fca7e44cb6cf7abf478cf7bbba5b32316aa
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2026-03-28 16:19:06 +0600

    feat: implement task rendering and status update with event delegation

--- todo-rawJs/index.js
+++ todo-rawJs/index.js
@@ -1,53 +1,85 @@
 function getFromLS() {
   const tasks = localStorage.getItem("tasks");
   const retrievedTasks = JSON.parse(tasks) || [];
-  console.log(retrievedTasks);
+  // console.log(retrievedTasks);
   return retrievedTasks;
 }
 
-function setToLS(newTask) {
-  const tasks = getFromLS();
-  const allTasks = [...tasks, newTask];
+function setToLS(allTasks) {
   localStorage.setItem("tasks", JSON.stringify(allTasks));
   showTasks();
 }
 
-function getTask() {
-  const taskInput = document.getElementById("taskInput").value;
+function getTaskInput() {
+  const taskInput = document.getElementById("taskInput");
   const task = {
     _id: crypto.randomUUID(),
-    taskName: taskInput,
+    taskName: taskInput.value.trim(),
     createdAt: new Date().toISOString(),
-    taskStatus: "incomplete",
+    isCompleted: false,
   };
 
   if (task.taskName) {
-    setToLS(task);
+    const tasks = getFromLS();
+    const allTasks = [...tasks, task];
+    // console.log(allTasks, "from get task input");
+    setToLS(allTasks);
+    taskInput.value = "";
   } else {
-    console.log("pls, input a valid name");
+    alert("pls, input a valid name");
   }
 }
 
+function updateTask(taskId) {
+  const tasks = getFromLS();
+  const updatedTasks = tasks.map((task) => {
+    if (task._id == taskId) {
+      return { ...task, isCompleted: !task.isCompleted };
+    }
+    return task;
+  });
+  // console.log(updatedTasks, "from update func");
+  setToLS(updatedTasks);
+}
+
+const taskList = document.getElementById("taskList");
+taskList.addEventListener("change", (e) => {
+  if (e.target.type === "checkbox") {
+    const fullId = e.target.id;
+    const taskId = fullId.replace("status-", "");
+    // console.log(taskId);
+    updateTask(taskId);
+  }
+});
+
 function showTasks() {
   const tasks = getFromLS();
-  const taskList = document.getElementById("taskList");
+  const fragment = document.createDocumentFragment();
+  // console.log(tasks, "from show task");
 
   taskList.innerHTML = "";
   tasks.forEach((task) => {
-    const checkedAttribute = task.taskStatus === "complete" ? "checked" : "";
+    const checkedAttribute = task.isCompleted === true ? "checked" : "";
     const taskItem = document.createElement("li");
+
     taskItem.innerHTML = `
     <div  class="flex justify-between">
         <div>
         <input id="status-${task._id}" ${checkedAttribute} type="checkbox"/>
-        <label for="status-${task._id}">${task.taskName}</label>
+        <label for="status-${task._id}" class="${task.isCompleted ? "line-through" : ""}">${task.taskName}</label>
         </div>
-        <span>${task.taskStatus}</span>
+        <span>${task.isCompleted == true ? "Complete" : "Incomplete"}</span>
     </div>
     `;
 
-    taskList.appendChild(taskItem);
+    // const checkbox = taskItem.querySelector(`#status-${task._id}`);
+    // checkbox.addEventListener("change", (e) => {
+    //   updateTask(task._id);
+    // });
+    fragment.appendChild(taskItem);
   });
+
+  taskList.appendChild(fragment);
 }
 
 showTasks();
